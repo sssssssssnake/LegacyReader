e3d77d7a971954f3ef169bb2f2ca6c3e82598574
sssssssssnake <samuel@mulinoit.com>
2024-06-06 15:26:49 -0700

    Added extraction methods (need to test)

---
+++ app/src/main/java/com/serpentech/legacyreader/filemanagement/ExtractMxl.java
@@ -0,0 +1,53 @@
+package com.serpentech.legacyreader.filemanagement;
+
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.BufferedOutputStream;
+
+public class ExtractMxl {
+
+
+    public static void unzip(String zipFilePath, String destDir) {
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if(!dir.exists()) dir.mkdirs();
+        try (ZipInputStream zipIn = new ZipInputStream(Files.newInputStream(Paths.get(zipFilePath)))) {
+            ZipEntry entry = zipIn.getNextEntry();
+            // iterates over entries in the zip file
+            while (entry != null) {
+                String filePath = destDir + File.separator + entry.getName();
+                if (!entry.isDirectory()) {
+                    // if the entry is a file, extracts it
+                    // ensure directories exist
+                    File outFile = new File(filePath);
+                    outFile.getParentFile().mkdirs();
+                    extractFile(zipIn, filePath);
+                } else {
+                    // if the entry is a directory, make the directory
+                    File dirToCreate = new File(filePath);
+                    dirToCreate.mkdirs();
+                }
+                zipIn.closeEntry();
+                entry = zipIn.getNextEntry();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+            byte[] bytesIn = new byte[4096];
+            int read = 0;
+            while ((read = zipIn.read(bytesIn)) != -1) {
+                bos.write(bytesIn, 0, read);
+            }
+        }
+    }
+
+}
