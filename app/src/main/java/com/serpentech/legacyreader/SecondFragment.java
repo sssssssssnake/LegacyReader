6c6507af74856638c6d2a270ac1986c9b605686c
sssssssssnake <samuel@mulinoit.com>
2024-06-06 15:26:17 -0700

    Moved things around TESTED

--- app/src/main/java/com/serpentech/legacyreader/SecondFragment.java
+++ app/src/main/java/com/serpentech/legacyreader/SecondFragment.java
@@ -26,6 +26,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.serpentech.legacyreader.databinding.FragmentSecondBinding;
 import com.serpentech.legacyreader.filemanagement.ConfigFilesJson;
 import com.serpentech.legacyreader.filemanagement.CustomFileManager;
+import com.serpentech.legacyreader.managedbuttoncode.ButtonsSecondFragment;
 
 
 public class SecondFragment extends Fragment {
@@ -55,32 +56,8 @@ public class SecondFragment extends Fragment {
                         if (data != null) {
                             Uri uri = data.getData();
                             assert uri != null;
-                            String name;
-
-                            // log the path to debug
-                            // Check if the real path is valid, otherwise use the URI string
-                            String uriPath = getRealPathFromURI(uri);
-                            if (uriPath == null) {
-                                uriPath = uri.toString();
-                            }
-                            try {
-                                uriPath = URLDecoder.decode(uriPath, StandardCharsets.UTF_8.name());
-                            } catch (UnsupportedEncodingException e) {
-                                throw new RuntimeException(e);
-                            }
-
-                            //grab the file name from the URI using last /
-                            name = uriPath.substring(uriPath.lastIndexOf("/") + 1);
-                            Log.d("FileStuff", "Selected file name: " + name);
-                            // log path
-                            Log.d("FileStuff", "Selected file path: " + uriPath);
-                            // if the file name does not contain a period, nullify the field
-                            if (!name.contains(".")) {
-                                name = null;
-                            }
-
-
-                            CustomFileManager.copyFileUri(getContext(), uri, name, uriPath);                            // Now you can use the path to manipulate the file
+                            ButtonsSecondFragment.grabUri(uri, getContext());
+                            // Now you can use the path to manipulate the file
 
                         }
                     }
@@ -88,11 +65,9 @@ public class SecondFragment extends Fragment {
         );
 
         binding.buttonSecond.setOnClickListener(v -> {
-            ConfigFilesJson.number = ConfigFilesJson.readBadFileNamesRecord();
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("*/*");
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            fileChooserLauncher.launch(intent);
+            ;
+
+            ButtonsSecondFragment.processFile(fileChooserLauncher, getContext());
         });
 
         binding.backButton.setOnClickListener(v ->
@@ -107,42 +82,9 @@ public class SecondFragment extends Fragment {
         binding = null;
     }
 
-    public String getRealPathFromURI(Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = getContext().getContentResolver().query(contentUri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } catch (Exception e) {
-            // This is the case when the quick pop-up provides a content URI
-            // which does not directly map to a file path
-            Log.e("FileSelection", "Could not get file path: " + e.getMessage());
-            return null;
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
 
-    public static String getFilePathForN(Uri uri, Context context) {
-        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        String document_id = cursor.getString(0);
-        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-        cursor.close();
 
-        cursor = context.getContentResolver().query(
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
-        cursor.moveToFirst();
-        @SuppressLint("Range") String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-        cursor.close();
 
-        return path;
-    }
 
 
 
---
+++ app/src/main/java/com/serpentech/legacyreader/managedbuttoncode/ButtonsSecondFragment.java
@@ -0,0 +1,79 @@
+package com.serpentech.legacyreader.managedbuttoncode;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.activity.result.ActivityResultLauncher;
+
+import com.serpentech.legacyreader.filemanagement.ConfigFilesJson;
+import com.serpentech.legacyreader.filemanagement.CustomFileManager;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+public class ButtonsSecondFragment {
+    public static void processFile(ActivityResultLauncher<Intent> launcher, Context context) {
+
+        ConfigFilesJson.number = ConfigFilesJson.readBadFileNamesRecord();
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        launcher.launch(intent);
+    }
+
+    public static void grabUri(Uri uri, Context context) {
+
+        String name;
+        // log the path to debug
+        // Check if the real path is valid, otherwise use the URI string
+        String uriPath = getRealPathFromURI(uri, context);
+        if (uriPath == null) {
+            uriPath = uri.toString();
+        }
+        try {
+            uriPath = URLDecoder.decode(uriPath, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        //grab the file name from the URI using last /
+        name = uriPath.substring(uriPath.lastIndexOf("/") + 1);
+        Log.d("FileStuff", "Selected file name: " + name);
+        // log path
+        Log.d("FileStuff", "Selected file path: " + uriPath);
+        // if the file name does not contain a period, nullify the field
+        if (!name.contains(".")) {
+            name = null;
+        }
+
+
+        CustomFileManager.copyFileUri(context, uri, name, uriPath);
+
+    }
+
+    public static String getRealPathFromURI(Uri contentUri, Context context) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } catch (Exception e) {
+            // This is the case when the quick pop-up provides a content URI
+            // which does not directly map to a file path
+            Log.e("FileSelection", "Could not get file path: " + e.getMessage());
+            return null;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+}
