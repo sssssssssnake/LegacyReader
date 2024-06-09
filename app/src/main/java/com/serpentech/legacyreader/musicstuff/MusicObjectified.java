9e3d430a6eb0aef0dd7f8d35ccf32f5956f4b524
sssssssssnake <samuel@mulinoit.com>
2024-06-09 16:28:09 -0700

    edited, now will organize

--- app/src/main/java/com/serpentech/legacyreader/musicstuff/MusicObjectified.java
+++ app/src/main/java/com/serpentech/legacyreader/musicstuff/MusicObjectified.java
@@ -29,26 +29,14 @@ public class MusicObjectified {
     public class Note{
         public int [] lengthFraction;
         public char letterName;
-        public int sharpCount;
-        public int flatCount;
+        public float alter;
         public int octave;
         public int positionFractionFromMeasureStart;
-
         public int centOffset;
 
-        public Note (int [] lengthFraction, char letterName, int octave, int sharp, int flat, int positionFractionFromMeasureStart) {
-            this.lengthFraction = lengthFraction;
-            this.letterName = letterName;
-            this.octave = octave;
-            this.positionFractionFromMeasureStart = positionFractionFromMeasureStart;
-            this.sharpCount = sharp;
-            this.flatCount = flat;
-        }
 
-        public Note (int [] lengthFraction, char letterName, int octave, int sharp, int flat, int positionFractionFromMeasureStart, int centOffset) {
-            this(lengthFraction, letterName, octave, sharp, flat, positionFractionFromMeasureStart);
-            this.centOffset = centOffset;
-        }
+
+
 
 
     }
