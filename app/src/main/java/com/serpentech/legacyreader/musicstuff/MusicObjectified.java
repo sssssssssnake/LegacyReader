f60f6bfdb15130d54c44027bbbcdb3f2c87e69d0
sssssssssnake <samuel@mulinoit.com>
2024-06-10 14:55:52 -0700

    update gradle and more prep (need to test)

--- app/src/main/java/com/serpentech/legacyreader/musicstuff/MusicObjectified.java
+++ app/src/main/java/com/serpentech/legacyreader/musicstuff/MusicObjectified.java
@@ -1,16 +1,20 @@
 package com.serpentech.legacyreader.musicstuff;
 
+import com.serpentech.legacyreader.filemanagement.xmlmanage.XmlGrab;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MusicObjectified {
     public class Measure {
+        public boolean hasAttributesl;
+        public int divisions;
         public int staves;
         public int measureNum;
         public int measureLength;
         public int[] measureType;
 
-        public List<SimpleNote> key;
+        public SimpleNote key;
         public List<Note> notes;
 
         public Measure (int measureNum, int measureLength, int[] measureType) {
@@ -24,17 +28,68 @@ public class MusicObjectified {
         public void addNote (Note note) {
             this.notes.add(note);
         }
+        public void addKey (SimpleNote key) {
+            this.key = key;
+        }
+        public Measure (String xmlMeasureContent) {
+            int measureNum = 0;
+
+            int measureLength;
+            int[] measureType;
+            XmlGrab xmlGrab = new XmlGrab();
+
+            // we need to grab the contents from an xml parcel
+            List<String[]> measureHeaders = xmlGrab.grabHeaders(xmlMeasureContent, "measure");
+            for (String[] header : measureHeaders) {
+                if (header[0].equals("number")) {
+                    measureNum = Integer.parseInt(header[1]);
+                }
+            }
+            String divisonsString = null;
+            if (xmlMeasureContent.contains("divisions")) {
+                divisonsString = xmlGrab.grabContents(xmlMeasureContent, "divisions");
+                divisions = Integer.parseInt(divisonsString);
+            } else {
+                divisions = 0;
+            }
+
+            String measureLengthString;
+            if (xmlMeasureContent.contains("beats")) {
+                measureLengthString = xmlGrab.grabContents(xmlMeasureContent, "beats");
+                measureLength = Integer.parseInt(measureLengthString);
+            } else {
+                measureLength = 0;
+            }
+
+            String measureTypeString;
+            if (xmlMeasureContent.contains("beat-type")) {
+                measureTypeString = xmlGrab.grabContents(xmlMeasureContent, "beat-type");
+                measureType = new int[]{measureLength, Integer.parseInt(measureTypeString)};
+            } else {
+                measureType = new int[]{0, 0};
+            }
+
+            this.measureNum = measureNum;
+            this.measureLength = measureLength;
+            this.measureType = measureType;
+            this.notes = new ArrayList<>();
+
+            System.gc();
+        }
     }
 
     public class Note{
         public int [] lengthFraction;
-        public char letterName;
-        public float alter;
-        public int octave;
+        SimpleNote pitch;
         public int positionFractionFromMeasureStart;
-        public int centOffset;
+        int staveNumber;
 
-
+        public Note (int[] lengthFraction, SimpleNote pitch, int positionFractionFromMeasureStart, int staveNumber) {
+            this.lengthFraction = lengthFraction;
+            this.pitch = pitch;
+            this.positionFractionFromMeasureStart = positionFractionFromMeasureStart;
+            this.staveNumber = staveNumber;
+        }
 
 
 
@@ -43,16 +98,18 @@ public class MusicObjectified {
     public class SimpleNote {
         public char letterName;
         public int octave;
-        public int sharpCount;
-        public int flatCount;
+        public float alter;
 
-        public SimpleNote (char letterName, int octave, int sharp, int flat) {
+        public SimpleNote (char letterName, int octave, int alter) {
             this.letterName = letterName;
             this.octave = octave;
-            this.sharpCount = sharp;
-            this.flatCount = flat;
+            this.alter = alter;
         }
 
+
     }
+
+
+
 
 }
